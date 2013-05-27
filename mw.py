20f6a19ae74017465893f65ff0a15a6ac8f30f13
atydnyuk <atydnyuk@gmail.com>
2013-05-27 15:51:14 -0400

    borrowed some menu code because mine was shit

--- mw.py
+++ mw.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python 
-# -*- coding: utf-8 -*- 
+
+## Credit where credit is due:
+
+################
+# The menu code:
+################
+# Topmenu and the submenus are based of the example 
+# found at this location 
+# http://blog.skeltonnetworks.com/2010/03/python-curses-custom-menu/
+# The rest of the work was done by Matthew Bennett and he 
+# requests you keep these two mentions when you reuse the code :-)
+# Basic code refactoring by Andrew Scheller
+
+
 import curses 
 from curses import panel
 import sys
@@ -10,7 +23,8 @@ def main():
     if len(sys.argv) == 2:
         display_single_entry(sys.argv[1])
     elif len(sys.argv) == 1:
-        display_menu()
+        processmenu(menu_data)
+        curses.endwin()
     else:
         print_usage()
         
@@ -18,80 +32,87 @@ def main():
 def display_single_entry(argument):
     print argument
 
-def display_menu():
+menu_data = {
+    'title': "Main Menu", 'subtitle': "The Main Nav Menu for the program",
+    'options': [
+        { 'title': "Lookup Term", 'command': '' },
+        { 'title': "About", 'command': '' },
+        ]
+    }
+
+# This function displays the appropriate menu and returns the option selected
+def runmenu(screen, menu, parent, n, h):
+  if parent is None:
+    lastoption = "Exit"
+  else:
+    lastoption = "Return to %s menu" % parent['title']
+
+  optioncount = len(menu['options']) # how many options in this menu
+
+  pos=0 #pos is the zero-based index of the hightlighted menu option.  Every time runmenu is called, position returns to 0, when runmenu ends the position is returned and tells the program what option has been selected
+  oldpos=None # used to prevent the screen being redrawn every time
+  x = None #control for while loop, let's you scroll through options until return key is pressed then returns pos to program
+  
+  # Loop until return key is pressed
+  while x !=ord('\n'):
+    if pos != oldpos:
+      oldpos = pos
+      screen.clear() #clears previous screen on key press and updates display based on pos
+      screen.border(0)
+      screen.addstr(2,2, menu['title'], curses.A_STANDOUT) # Title for this menu
+      screen.addstr(4,2, menu['subtitle'], curses.A_BOLD) #Subtitle for this menu
+
+      # Display all the menu items, showing the 'pos' item highlighted
+      for index in range(optioncount):
+        textstyle = n
+        if pos==index:
+          textstyle = h
+        screen.addstr(5+index,4, "%d - %s" % (index+1, menu['options'][index]['title']), textstyle)
+      # Now display Exit/Return at bottom of menu
+      textstyle = n
+      if pos==optioncount:
+        textstyle = h
+      screen.addstr(5+optioncount,4, "%d - %s" % (optioncount+1, lastoption), textstyle)
+      screen.refresh()
+      # finished updating screen
+
+    x = screen.getch() # Gets user input
+
+    # What is user input?
+    if x >= ord('1') and x <= ord(str(optioncount+1)):
+      pos = x - ord('0') - 1 # convert keypress back to a number, then subtract 1 to get index
+    elif x == 258: # down arrow
+      if pos < optioncount:
+          pos += 1
+      else: pos = 0
+    elif x == 259: # up arrow
+      if pos > 0:
+            pos += -1
+      else: pos = optioncount
+    elif x != ord('\n'):
+      curses.flash()
+
+  # return index of the selected item
+  return pos
+
+# This function calls showmenu and then acts on the selected item
+def processmenu(menu, parent=None):
     screen = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
     curses.start_color()
-    curses.noecho() 
-    curses.curs_set(0) 
     screen.keypad(1) 
-    
-    highlight = 0
-    choices = {"Lookup Term", "About","Exit"}
-    num_choices = 3
-    print_menu(screen,highlight,choices,num_choices)
+    curses.init_pair(1,curses.COLOR_BLACK, curses.COLOR_WHITE) 
+    h = curses.color_pair(1) 
+    n = curses.A_NORMAL 
+    optioncount = len(menu['options'])
+    exitmenu = False
+    while not exitmenu: #Loop until the user exits the menu
+        getin = runmenu(screen, menu, parent, n, h)
+        if getin == optioncount:
+            exitmenu = True
 
-    while True: 
-        event = screen.getch() 
-        if event == ord("q"): 
-            break 
-        elif event == curses.KEY_UP:
-            if (highlight==0):
-                highlight = num_choices-1
-            else:
-                highlight -= 1
-        elif event == curses.KEY_DOWN:
-            if (highlight==num_choices-1):
-                highlight = 0
-            else:
-                highlight += 1
-        else:
-            screen.addstr(str(event))
-        screen.refresh()
-        print_menu(screen,highlight,choices,num_choices)
-    curses.nocbreak()
-    screen.keypad(0)
-    curses.echo()
-    curses.endwin()
-
-def mkpanel(color, rows, cols, tly, tlx):
-    win = curses.newwin(rows, cols, tly, tlx)
-    pan = panel.new_panel(win)
-    if curses.has_colors():
-        if color == curses.COLOR_BLUE:
-            fg = curses.COLOR_WHITE
-        else:
-            fg = curses.COLOR_BLACK
-        bg = color
-        curses.init_pair(color, fg, bg)
-        win.bkgdset(ord(' '), curses.color_pair(color))
-    else:
-        win.bkgdset(ord(' '), curses.A_BOLD)
-
-    return pan
-
-def print_menu(screen, highlight, choices, num_choices):
-    i=0
-    x = 0
-    y = 0
-    dy = 30
-    p1 = mkpanel(curses.COLOR_RED,
-                 curses.LINES,
-                 curses.COLS,
-                 0,
-                 0)
-    p1.set_userptr("p1")
-    #fill_panel(p1)
-    for choice in choices:
-        if (i!=highlight):
-            #p1.move(y,0)
-            screen.addstr(y,x,choice)
-        else:
-            #p1.move(y,0)
-            #p1.attron(curses.A_BOLD)
-            screen.addstr(y,x,choice,curses.A_BOLD)
-            #p1.attroff(curses.A_BOLD)
-        i+=1
-        y+=dy
     
 def print_usage():
     print "Usage of ManWiki:"
